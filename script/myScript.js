360819c2d9a0321bec50a047d1e389ea6de27e8d
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-20 14:24:37 +0100

    Calcola Prezzo del Biglietto (html)

---
+++ script/myScript.js
@@ -0,0 +1,31 @@
+/*
+Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L’output del prezzo finale avrà massimo due decimali
+*/
+
+// Es. 
+
+// Età
+
+
+// Nr. Km
+
+
+// Prezzo del biglietto (default)
+// let price = (0.21 * km);
+
+
+// // Tariffe agevolate
+// if (age < 18) {
+//   price = ((0.21 * km) * 0.8);
+
+// } else if (age > 65) {
+//   price = ((0.21 * km) * 0.6);
+
+// }
+
+
